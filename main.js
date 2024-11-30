276421cf265ba4a99d22c246104325c35388b82c
unknown <rafalexwtf@gmail.com>
2024-11-30 16:04:42 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+// // Variables
+// // let MyName = "Raul"
+// //     MyPgae = "23"
+
+// // alert(MyName);
+// // alert(MyPgae)
+
+
+// //Constante 
+// // const MyBirthday = "16/03/07"
+// // // alert(MyBirthday);
+// let MyPgae = 33;
+// let IsActive = true;
+// let IsBlue = false;
+
+// let MyHight = null;
+// let MyHight2 = undefined;
+
+// // INGRESO DE ARGUMENTOS 
+// let year = prompt('año actual: ');
+// // alert(year);
+
+// // confirm
+// let isBoss = confirm("eres el jefe?");
+// // alert(isBoss);
+
+// //operadores matematicos 
+// let Num1 = 10;
+// let Num2 = 20;
+
+// // alert( Num1 + Num2)
+// // alert( Num1 - Num2)
+// // alert( Num1 * Num2)
+// // alert( Num1 / Num2)
+
+// let Num3 = 2;
+// Num3++;
+// alert(Num3);
+
+// operadores de comparacion 
+
+alert( 2 > 1 );
+alert( 2 < 1) ; 
+alert( 2 == 1); 
+alert( 2 != 1)
+
+// condicionales 
+
