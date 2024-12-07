62e1247ca84bd4925537937f457194c5658eb45a
unknown <rafalexwtf@gmail.com>
2024-12-07 16:07:54 -0400

    ciclos y funciones

--- main.js
+++ main.js
@@ -37,23 +37,128 @@
 // Num3++;
 // alert(Num3);
 
-// operadores de comparacion 
+// // operadores de comparacion 
 
-alert( 2 > 1 );
-alert( 2 < 1) ; 
-alert( 2 == 1); 
-alert( 2 != 1)
+// alert( 2 > 1 );
+// alert( 2 < 1) ; 
+// alert( 2 == 1); 
+// alert( 2 != 1)
 
-// condicionales 
+// // condicionales 
 
-let year = prompt(" ingrese el año actual:");
-if (year == 2024) {
-    alert ("Muy Bien ");
-    alert ( " eres genial ");
-} else if ( year < 2024){
-    alert ("Muy bajo")
-}else if ( year > 2024){
-    alert ("Muy alto")
-}else {
-    alert ("No es correcto")
+// let year = prompt(" ingrese el año actual:");
+// if (year == 2024) {
+//     alert ("Muy Bien ");
+//     alert ( " eres genial ");
+// } else if ( year < 2024){
+//     alert ("Muy bajo")
+// }else if ( year > 2024){
+//     alert ("Muy alto")
+// }else {
+//     alert ("No es correcto")
+// }
+
+// operadores logicos 
+// or || 
+// alert( true || true); // true
+// alert( true || false); //true
+// alert( false || true); // true
+// alert( false || false); //false
+
+
+// let hour = 12;
+// let isWeekend = true;
+
+// if (hour < 9 || hour >18 || isWeekend){
+//     alert (" La oficina esta cerrada ")
+// } else {
+//     alert("open!");
+// }
+
+// and &&
+// alert (true && true); // true
+// alert (true && false); // true
+// alert (false && true); // true
+// alert (false && false); // true
+// let admin1
+// let user = prompt(" ingrese su nombre");
+// if(user === "admin") {
+//         let pass = prompt("Ingrese su contraseña")
+//         if (pass === "1234567890")
+//             alert("Bienvenido")
+//         else 
+//             alert("contraseña incorrecta")
+//     // } else if ( pass < 16032007){
+//     //     alert ("Contraseña incorrecta")
+//     // }else if ( year > 16032007){
+//     //     alert ("Contraseña incorrecta")
+//     // }else if ( pass = 16032007){
+//     //     alert ("Contraseña incorrecta")
+//     // }else {
+//     //     alert ("La contraseña es incorrecto ")
+//     // }
+// }else
+//     alert("no se quien eres")
+
+
+
+// let year = prompt(" ingrese el año actual:");
+// if (year == 2024) {
+//     alert ("Muy Bien ");
+//     alert ( " eres genial ");
+// } else if ( year < 2024){
+//     alert ("Muy bajo")
+// }else if ( year > 2024){
+//     alert ("Muy alto")
+// }else {
+//     alert ("No es correcto")
+// }
+
+// // ciclos while 
+// while(condicion){
+//         sentences;
+//         sentences;
+// }
+
+
+// let counter = 0;
+
+// while (counter < 3){
+//     alert (counter)
+//     counter++;
+// }
+
+// ciclo for 
+// for (ByteLengthQueuingStrategy; CSSConditionRule, step); {
+//     // for body
+// }
+
+// for ( let counter =0; counter < 3; counter++) {
+//     alert(counter);
+// }
+
+// funciones
+// function showMessage(){
+//     alert("hola mundo");
+// }
+// showMessage();
+// showMessage();
+
+// funciones con variables locales 
+// funciones
+// function showMessage(){
+//     let message = "epale :)"
+//     alert(message);
+// }
+// showMessage();
+// alert(message);
+
+// funciones con variables externas
+let userName = "juan";
+
+function showMessage(){
+    let message = "epale :) " + userName;
+    alert(message);
 }
+
+showMessage();
